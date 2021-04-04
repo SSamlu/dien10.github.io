5003dee1946b2b2a2911f45df5c1fa6665a1346b
SSamlu <nguyendinhdien19980@gmail.com>
2021-04-04 17:49:50 +0700

    first commit

--- global.js
+++ global.js
@@ -1,51 +1,16 @@
 $(document).ready(function(){
-    console.log("dien");
-    $("#form").validate(
-
-    );
+    $("#form").validate();
     $('.submit').on('click',function(e){
         e.preventDefault();
-        console.log("dien");
-        // if(name==""||phone==""||photo==""||email==""){
-        //   alert("nhập thông tin cồn thiếu");
-        // }
-      validation = ()=>{
-          console.log("okk");
-      }
-      
-        var formdata={
-            name : $('#fullname').val(),
-            phone : $('#phone').val(),
-            photo : $('#photo').val(),
-            email : $('#email').val(),
-          };
             console.log(formdata);
             $.ajax({
               url: 'https://freemind-test.netlify.app/.netlify/functions/test',
               type: "POST",
-              data: JSON.stringify(formdata),
+              data: $('form').serialize(),
               Headers:{'content-Type':'application/json'},
-              // encode:true,
               success: function (result){
-                // $('#hienthi').html('result');
                 // alert(result);
             }
             })
     })
 })
-
-// var xhr = new XMLHttpRequest();
-// console.log(xhr);
-// xhr.open("post","https://freemind-test.netlify.app/.netlify/functions/test",true);
-// xhr.setRequestHeader("content-Type","application/json;charset=UTF-8")
-// xhr.onload = function(){
-//     if(this.status==200){
-//         var response = xhr.response;
-//         var parsedData = JSON.parse(response);
-//         console.log(typeof response);
-//     }
-//     if(this.status == 502){
-//         console.log("notFound")
-//     }
-// }
-// xhr.send();
